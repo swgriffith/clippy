75465a55b97011cd30a0cfbea236132c78e7a382
Steve Griffith <steve.griffith@microsoft.com>
2018-07-23 22:05:05 -0400

    initial commit

---
+++ src/Controllers/clippyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace clippyAPI.Controllers
+{
+    public class clippyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("api/clippy")]
+        public string ClippyIt()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                string text = reader.ReadToEndAsync().Result.ToString();
+                string top = "";
+                string bottom= "";
+                for (int i = 0; i < text.Length+2; i++)
+                {
+                    top +="_";
+                    bottom+="-";
+                }
+                string output = $" {top}\n< {text} >\n {bottom}\n \\\n  \\\n    __\n   /  \\\n   |  |\n   @  @\n   |  |\n   || |/\n   || ||\n   |\\_/|\n   \\___/ ";
+
+                return output;
+            }
+        }
+    }
+}
